909de0ad00cd1d72e2e6068b98a6792771f3acc4
Antti Turunen <asturune@hotmail.com>
2021-02-18 20:04:15 +0200

    login ja logout lisätty

--- src/App.js
+++ src/App.js
@@ -1,22 +1,166 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+//Login pyyntö 
+import loginService from './services/login'
+
+
+//Tällä muotoillaan notificaatio
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  //Tilat usernamelle ja salasanalle
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [errorMessage, setErrorMessage] = useState('')
 
+  //Effect hook hakemaan kaikki blogit kun sivu ladataan/uudelleen ladataan
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
+
+  console.log(blogs.map(blog => blog.user.id))
+  console.log(user)
+  //Effect hook hakemaan locla storagesta loggautuneen käyttäjän tiedot
+  //ettei pyydä sivun uudelleen latauksen yhteydessä uudelleen kirjautumaan
+  //Efektin lopuussa parametrina oleva tyhjä taulukko "[]" varmistaa sen, että efekti suoritetaan 
+  //ainoastaan kun komponentti renderöidään ensimmäistä kertaa
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  //----------------------- LOGOUT JA LOGIN-----------------------------------
+  //Kirjautumislomakkeen lähettämisestä vastaava metodi
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    //console.log('logging in with', username, password)
+
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      //Kirjautuneen käyttäjän tiedot local storageen, jotta pysyy aktiivisena
+      //koko session ajan eikä esim. sivun uudelleen lataamisen yhteydessä pyydä
+      //kirjautumaan uudelleen
+      //Käyttäjän tietoja pystyy nyt tarkastella selaimen konsolista
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+
+      console.log('LOGGAUTUNUT KÄYTTÄJÄ', user)
+      blogService.setToken(user.token)
+      setUser(user)
+      //Nollataan kirjautumislomakkeen kentät, kun login nappia painettu
+      setUsername('')
+      setPassword('')
+
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+
+  //Tämä sitä varten, että voidaan renderöidä ehdollisesti eli jos ei ole loggautunut
+  //Näytetään login formi, muutoin blogiformi
+  const loginForm = () => (
+
+    <form onSubmit={handleLogin}>
+      <div>
+        <h2>Login to apllication</h2>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+  //Logout napin lähettämisestä vastaava metodi
+  const handleLogout = (event) => {
+    //Pitää tyhjätä selaimen loclastoragesta käyttäjä, jottei sivua uudelleen
+    //ladattaessa taas olla loggautuneena
+    window.localStorage.clear()
+    //Tyhjätään token
+    blogService.setToken('')
+    //Tyhjätään käyttäjä
+    setUser(null)
+  }
+  //Logoutnapin renderöinti
+  const logoutForm = () => (
+    <button onClick={handleLogout}>logout</button>
+  )
+
+  //----------------------- LOGOUT JA LOGIN LOPPUU-----------------------------------
+
+  //-----------------UUDEN BLOGIN LUOMINEN-------------------------------------------
+  //Uuden blogin luomiseen renderöity formi
+  const blogForm = () => (
+    /*
+    <form onSubmit={addBlog}>
+      <input
+        value={newBlog}
+        onChange={handleBlogChange}
+      />
+      <button type="submit">save</button>
+    </form>
+    */
+    <div>
+      HIENOA LOGGAUDUIT!!
+    </div>
+  )
+  //-----------------UUDEN BLOGIN LUOMINEN LOPPUU-------------------------------------------
+
+
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+
+      <Notification message={errorMessage} />
+
+      {user === null ?
+        loginForm() :
+        <div>
+          <p>{user.name} logged in {logoutForm()}</p>
+          {blogForm()}
+          <h2>YOUR BLOGS</h2>
+
+          {blogs.filter(blog => blog.user.username === user.username).map(blog =>
+            <Blog key={blog.id} blog={blog} />)}
+        </div>
+      }
     </div>
   )
 }
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,26 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+//Moduulille on määritelty vain moduulin sisällä näkyvä muuttuja token, 
+//jolle voidaan asettaa arvo moduulin exporttaamalla funktiolla setToken
+const setToken = newToken => {
+  token = `bearer ${newToken}`
+}
+//Haetaan kaikki blogit
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
+//Luodaan uusi blogi
+//Async/await-syntaksi asettaa moduulin tallessa pitämän 
+//tokenin Authorization-headeriin, jonka se antaa axiosille metodin post kolmantena parametrina
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+}
 
-export default { getAll }
+export default { getAll, create, setToken }
